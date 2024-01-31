f4cef62d8fbd8089ad7e16fadbea953cf9102f1c
nadiamartel <nadiagmartel@gmail.com>
2024-01-31 18:01:53 -0300

    buttons + icons

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,18 @@
+import upload from "../assets/uploadicon.png";
+import options from "../assets/dotsicon.png";
 
 const Card = ({ item }) => {
     return (
         <div key={item.id} className='item'>
             <div className='imagen'>
                 <img src={item.urls.small} alt={item.description} />
+                <a className="btn-save" href="">Guardar</a>
+                <a className="icon-upload" href="">
+                    <img src={upload} alt="upload" />
+                </a>
+                <a className="icon-dots" href="">
+                    <img src={options} alt="options" />
+                </a>
             </div>
             <p>{item.description}</p>
             <div>
