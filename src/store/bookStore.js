dbd1a5dc769bc06d70844cf562f4c2ad65d081c8
nadiamartel <nadiagmartel@gmail.com>
2024-02-22 21:46:53 -0300

    fan river

--- src/store/bookStore.js
+++ src/store/bookStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 
 //a esto lo copio tal cual de la docu
 export const useBookStore = create((set) => ({
-    value: "cat", //estado global
+    value: "river plate", //estado global
     updateValue: (newValue) => set({value: newValue})
   }))
 
